59c1976f02725bce69e1d66bde93385972eb4ebc
“JuanDiaz” <juanluisdiazguerrero@gmail.com>
2022-07-31 23:34:38 -0500

    NothingSelectedView

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,15 +1,11 @@
 import { Typography } from "@mui/material";
 import { JournalLayout } from "../layout/JournalLayout";
+import { NothingSelectedView } from "../view";
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <Typography>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque iure
-        veritatis nam adipisci molestiae ex corporis delectus sequi dolore
-        voluptatem placeat eum amet repellat dolores possimus, praesentium vel
-        libero ad.
-      </Typography>
+      <NothingSelectedView />
     </JournalLayout>
   );
 };
