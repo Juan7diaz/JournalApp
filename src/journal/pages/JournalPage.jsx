4ad0cb3e05515d32acdbcbdd1c3d2c1f0a85eacd
“JuanDiaz” <juanluisdiazguerrero@gmail.com>
2022-07-31 22:48:48 -0500

    NavBar

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,9 +1,15 @@
-import { Typography } from '@mui/material'
+import { Typography } from "@mui/material";
+import { JournalLayout } from "../layout/JournalLayout";
 
 export const JournalPage = () => {
   return (
-    <>
-      <Typography variant="h1">JournalPage</Typography>
-    </>
-  )
-}
+    <JournalLayout>
+      <Typography>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque iure
+        veritatis nam adipisci molestiae ex corporis delectus sequi dolore
+        voluptatem placeat eum amet repellat dolores possimus, praesentium vel
+        libero ad.
+      </Typography>
+    </JournalLayout>
+  );
+};
