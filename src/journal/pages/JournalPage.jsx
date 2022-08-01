9f52cd409918d198011b059f341f2b47a24f5e8a
“JuanDiaz” <juanluisdiazguerrero@gmail.com>
2022-08-01 00:19:53 -0500

    boton flotante

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,12 +1,26 @@
-import { Typography } from "@mui/material";
+import { AddOutlined } from "@mui/icons-material";
+import { IconButton, Typography } from "@mui/material";
 import { JournalLayout } from "../layout/JournalLayout";
 import { NoteView, NothingSelectedView } from "../view";
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      {/* <NothingSelectedView /> */}
-      <NoteView />
+      <NothingSelectedView />
+      {/* <NoteView /> */}
+      <IconButton
+        size="large"
+        sx={{
+          color: "white",
+          backgroundColor: "error.main",
+          ":hover": { backgroundColor: "error.main", opacity: 0.9 },
+          position: "fixed",
+          right: 50,
+          bottom: 50,
+        }}
+      >
+        <AddOutlined sx={{ fontSize: 20 }} />
+      </IconButton>
     </JournalLayout>
   );
 };
