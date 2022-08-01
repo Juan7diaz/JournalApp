26bcdbcf9b035fd36d7e42b4060f5d1504cd4c14
“JuanDiaz” <juanluisdiazguerrero@gmail.com>
2022-07-31 22:12:23 -0500

    RegisterPage

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,3 +1,57 @@
+import { Link } from "react-router-dom";
+import {
+  Button,
+  Grid,
+  Link as LinkMUI,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { AuthLayout } from "../layout/AuthLayout";
+
 export const RegisterPage = () => {
-  return <div>RegisterPage</div>
-}
+  return (
+    <AuthLayout title={"Crear Cuenta"}>
+      <form>
+        <Grid container>
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label="Nombre completo"
+              type="text"
+              placeholder="Tu nombre"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label="correo"
+              type="email"
+              placeholder="correo@googme.com"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label="contraseña"
+              type="password"
+              placeholder="*****"
+              fullWidth
+            />
+          </Grid>
+          <Grid container spacing={2} sx={{ mb: 2, mt: 0.5 }}>
+            <Grid item xs={12}>
+              <Button variant="contained" fullWidth>
+                Login
+              </Button>
+            </Grid>
+          </Grid>
+          <Grid container direction="row" justifyContent="end">
+            <Typography sx={{ mr: 1 }}>¿Ya tienes cuenta?</Typography>
+            <LinkMUI component={Link} to="/auth/login">
+              Ingresar
+            </LinkMUI>
+          </Grid>
+        </Grid>
+      </form>
+    </AuthLayout>
+  );
+};
